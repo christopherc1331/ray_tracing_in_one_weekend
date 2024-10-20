a8b0d4b15355d309ad2d8afdc83ae487c4efb875
Chris Cruz <christopherc1331@gmail.com>
2024-10-20 15:09:23 -0500

    fixed typo

--- src/ray.rs
+++ src/ray.rs
@@ -38,9 +38,9 @@ pub fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
     let b = -2f64 * dot(*ray.direction(), oc);
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4f64 * a * c;
-    match discriminant < 1f64 {
+    match discriminant < 0f64 {
         true => -1f64,
-        false => (-b - discriminant.sqrt()) / 2f64 * a,
+        false => (-b - discriminant.sqrt()) / (2f64 * a),
     }
 }
 
@@ -49,7 +49,7 @@ pub fn ray_color(ray: &Ray) -> Color {
     let t: f64 = hit_sphere(&Point3::new(0f64, 0f64, -1f64), 0.5f64, ray);
     if t > 0f64 {
         let n: Vec3 = unit_vector(ray.at(t) - Vec3::new(0f64, 0f64, -1f64));
-        return 0.5 * Color::new(n.x() + 1f64, n.y() + 0f64, n.z() + 0f64);
+        return 0.5 * Color::new(n.x() + 1f64, n.y() + 1f64, n.z() + 1f64);
     }
     let unit_direction: Vec3 = unit_vector(*ray.direction());
     let a = 0.5 * (unit_direction.y() + 1f64);
