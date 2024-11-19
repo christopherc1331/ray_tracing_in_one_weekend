f3fadf1000c0003158c7bf2e722033443dc939b2
Christopher Cruz <christopherc1331@gmail.com>
2024-11-18 22:05:26 -0600

    add rand crate and create util func for generating random floats in range and between 0 and 1

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ use crate::{hittable::HittableType, hittable_list::HittableList, sphere::Sphere}
 fn main() {
     let mut world: HittableList = HittableList::default();
     let sphere_1: HittableType =
-        hittable::HittableType::Sphere(Sphere::new(&Point3::new(0f64, 0f64, -1f64), 0.5f64));
+        HittableType::Sphere(Sphere::new(&Point3::new(0f64, 0f64, -1f64), 0.5f64));
     let sphere_2: HittableType =
-        hittable::HittableType::Sphere(Sphere::new(&Point3::new(0f64, -100.5f64, -1f64), 100f64));
+        HittableType::Sphere(Sphere::new(&Point3::new(0f64, -100.5f64, -1f64), 100f64));
     world.add(&sphere_2);
     world.add(&sphere_1);
 
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,13 @@
+use rand::prelude::*;
 use std::f64::consts::PI;
-
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180f64
+}
+
+pub fn random_double() -> f64 {
+    thread_rng().gen()
+}
+
+pub fn random_double_range(min: f64, max: f64) -> f64 {
+    thread_rng().gen_range(min..max)
 }
