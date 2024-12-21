4d49bb0f0fec55f79c97d7446836f0eb0d8db79f
Chris Cruz <christopherc1331@gmail.com>
2024-12-20 18:57:50 -0600

    add refraction function for different materials

--- src/vec3.rs
+++ src/vec3.rs
@@ -176,3 +176,10 @@ pub fn random_on_hemisphere(normal: Vec3) -> Vec3 {
 pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
     *v - 2f64 * dot(*v, *n) * *n
 }
+
+pub fn refract(uv: &Vec3, n: &Vec3, etai_over_etat: f64) -> Vec3 {
+    let cos_theta: f64 = dot(-*uv, *n).min(1f64);
+    let r_out_perp: Vec3 = etai_over_etat * (*uv + cos_theta * *n);
+    let r_out_prep_parallel: Vec3 = -(1f64 - r_out_perp.length_squared()).abs().sqrt() * *n;
+    r_out_perp - r_out_prep_parallel
+}
